6c9e3bcdd00d499f7a392b5d0c34b80e0cb910e2
AaronAmasi265 <85727132+BinaryAaron265@users.noreply.github.com>
2023-05-09 12:24:33 -0700

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,1 +1,18 @@
 
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Created by Aaron Amasi on 7.05.2023.
+ */
+public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.println(randomizedQueue.dequeue());
+        }
+    }
+}
